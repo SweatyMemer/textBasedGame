519fd81fd1a9062ffe8c96ff1537c1397e17a23e
SHEPPARD <dshep67@eq.edu.au>
2022-07-29 20:26:44 +1000

    added combat system, it isn't done yet though as the game doesn't register when all enemies are dead and as such doesn't finish the minigame, meaning the enemies keep attacking you after they're dead. So yeah that needs to be fixed.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+from array import array
+from ast import ListComp
+from re import M
+from sre_constants import CH_LOCALE
+from typing import List
 import colorama as col
 import imageToAscii
 from time import sleep
 from random import randint
 from os import system
 from time import time
+from itertools import zip_longest
+from numpy import concatenate
 col.init(autoreset=True)
 
 class imageClass:
     xavier = 0
     dudong = 1
-
+    night = 2
+    monster = 3
 
 
 def pogLog(*aa):
@@ -44,7 +52,6 @@ def displayImageRange(asciiImageList, start, stop, step=1, colour = col.Fore.WHI
         i += step
 
 class school:
-    
     def __init__(self):
         subject = randint(0,2)
         if subject == 0:
@@ -88,16 +95,39 @@ class school:
                 print(f"Your answer is correct, but you took too long to answer, you have 5 seconds to answer.")
                 return False
 
+class Weapon:
+    def __init__(self, damage: int, PP: int):
+        self.damage = damage
+        self.PP = PP        
+
 class Character:
-    def __init__(self, maxPP: int, PP: int, maxHP: int, HP: int, inventory: dict):
+    def __init__(self, maxPP: int, PP: int, maxHP: int, HP: int, defence: int, inventory: dict, friendly: bool, MC: bool, equippedWeapon: Weapon, alive: bool, name: str):
         self.maxPP = maxPP
         self.PP = PP
         self.maxHP = maxHP
         self.HP = HP
+        self.defence = defence
         self.inventory = inventory
+        self.MC = MC
+        self.friendly = friendly
+        self.equippedWeapon = equippedWeapon
+        self.alive = alive
+        self.name = name
     
+    def takeDamage(self, damage: int):
+        if randint(0,(self.defence/10)) == 0:
+            self.HP -= damage
+            print(f"{self.name} took {damage} damage")
+        else:
+            self.HP -= (damage/2)
+            print(f"{self.name} took {damage/2} damage")
+            
+    def takeTruedamage(self, damage: int):
+        self.HP -= damage
+        print(f"{self.name} took {damage} damage")
+    def heal(self, heal: int):
+        self.HP += heal
     
-
 class day:
     def __init__(self, apparition: bool, eliteApparition: bool = False):
         self.apparition = apparition
@@ -116,23 +146,158 @@ def wait(time: int):
         print(".", end="")
     print("")
 
+def print_lines(*lines: str):
+    """
+    A helpful function for printing many
+    separate strings on separate lines.
+    """
+    print("\n".join([line for line in lines]))
+
+class battle:
+    def __init__(self, *characters: Character) -> None:
+        pass
+    
+    def start(self, *characters: Character):
+        
+        self.friendlyCharacters = []
+        self.hostileCharacters = []
+
+        for character in characters:
+            if character.friendly == True:
+                self.friendlyCharacters.append(character)
+            else:
+                self.hostileCharacters.append(character)
+        
+        whoGoesFirst = randint(0,1)
+        wait(2)
+        while 1:
+            for fC in self.friendlyCharacters:
+                if whoGoesFirst == 0: #  if the coinflip returns 0 then it skips the friendlies turn for the first round, making the hostiles go first
+                    whoGoesFirst =  1
+                    break
+                print("Your party Goes first! ")
+                if fC.MC == True: #  if the friendly character is the main character give them some choices on the attack
+                    print("What do you want to do?")
+                    print_lines(
+                        f"1 - Basic Attack: {fC.equippedWeapon.damage}",
+                        f"2 - Psychic Attack: {fC.equippedWeapon.PP}",
+                        f"3 - Analyse - Grants increased critical chance for the next turn",
+                        f"HP: {fC.HP} | PP: {fC.PP}"
+                    )
+                    IN = input("> ")
+                    if IN == "1":
+                        for hC in self.hostileCharacters:
+                            if hC.alive == True:
+                                hC.takeDamage(fC.equippedWeapon.damage)
+                                if hC.HP <= 0:
+                                    hC.alive = False
+                                    print(f"{hC.name} has been defeated!")
+                                break
+                            else:
+                                pass
+                    elif IN == "2":
+                        for hC in self.hostileCharacters:
+                            if fC.PP > fC.equippedWeapon.PP/2:
+                                if hC.alive == True:
+                                    hC.takeTruedamage(fC.equippedWeapon.PP)
+                                    fC.PP -= fC.equippedWeapon.PP/2
+                                    if hC.HP <= 0:
+                                        hC.alive = False
+                                        print(f"{hC.name} has been defeated!")
+                                    break
+                                else:
+                                    pass
+                            else:
+                                print(f"You don't have enough PP to use this attack, {fC.PP} when you need {fC.equippedWeapon.PP/2}")
+                    elif IN == "3":
+                        print("Increased critical chance for next turn. RN this is complete cap tho") 
+                                                       
+                else: #  if the character isn't a main character we just do the damage that their weapon does
+                    for hC in self.hostileCharacters:
+                        if hC.alive == True:
+                            hC.takedamage(fC.equippedWeapon.damage)
+                            if hC.HP <= 0:
+                                hC.alive = False
+                                print(f"{hC.name} has been defeated!")
+                            break
+                        else:
+                            pass
+                    
+                    result = all(hC.alive == False for hC in self.hostileCharacters)
+                    if result:
+                        print("You have defeated all the enemies!")
+                        return True #  if all enemies are dead return True, a victory
+                    else: 
+                        pass
+            for hC in self.hostileCharacters:
+                for fC in self.friendlyCharacters:
+                    if fC.alive == True:
+                        fC.takeDamage(hC.equippedWeapon.damage)
+                        if fC.HP <= 0:
+                            fC.alive = False
+                            print(f"{fC.name} has been defeated!")
+                        break
+                    else: 
+                        pass
+                result = all(fC.alive == False for fC in self.friendlyCharacters)
+                if result:
+                    print("Your party has been defeated!")
+                    return False #  if all friendlies are dead return False, a failure
+                else: 
+                    pass
+    
+    def printBattle(self, friendlyCharacters: List[Character], hostileCharacters: List[Character]):
+        for friendlychar, hostilechar in zip_longest(friendlyCharacters, hostileCharacters):
+            print(f"""{friendlychar.HP} {' '*20} {hostilechar.HP}""")
+
+
 def main():
-    displayImage(getStoredAscii()[imageClass.dudong])
+    imageArray = getStoredAscii()
+    
+    displayImage(imageArray[imageClass.dudong])
     sleep(3)
     imageToAscii.terminalDefault()
     
-    MC = Character(100,100,100,100,{})
+    currentDay = day(True, False)
+    
+    MainCharacter = Character(100,100,100,100,10,{"Cursed Sword": 1},True,True,Weapon(20,20),True, input("What is your name? "))
     print("You have recently moved to a new school for study, and you have a free period and decide to go straight to your dorm.\nYou walk into your dorm and see a student who introduces himself to you as your roommate, Xavier")
     input(f"{col.Fore.YELLOW}Press any key to continue")
     system('cls')
     print("You begin your school day")
     wait(2)
-    if school():
-        MC.maxPP += 10
-        MC.PP += 10
+    if school() == True:
+        MainCharacter.maxPP += 10
+        MainCharacter.PP += 10
         print("You have gained 10 PP")
-    input(vars(MC))
+    
+    if currentDay.apparition:
+        print("You hear rumours around the school about a missing student, nobody knows where they are")
+        wait(3)
+        system('cls')
+    print("You decide to retire early as it's your first day and you are tired.")
+    wait(2)
+    displayImage(imageArray[imageClass.night])
+    sleep(8)
+    imageToAscii.terminalDefault()
+    
+    if currentDay.apparition:
+        print_lines(
+            "You are awoken by a demonic looking apparition, sitting at the foot of your bed",
+            "Your roommate Xavier is standing staring at the monster, a knife in hand."
+        )
+        wait(3)
+        displayImage(imageArray[imageClass.monster])
+        sleep(0.4)
+        imageToAscii.terminalDefault()
+        print("Battle Starting", end="")
+        enemy = Character(100,100,100,100,10,{},False,False,Weapon(10,10),True, "Thicc enemy dumptruck")
+        wait(2)
+        battle().start(MainCharacter, enemy)
+        
+        
+    input(vars(MainCharacter))
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
     main()
     imageToAscii.terminalDefault()
