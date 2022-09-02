9535405c1f271e35ffa6b737f2584f6f2c5f8cbc
SweatyMemer <ds123shep@gmail.com>
2022-09-02 12:31:04 +1000

    added item drops from the enemies, and added a shop where you can buy and sell items. Added a music minigame in addition to the maths minigame for the school part. Started working on the main game loop. It's all commented out in the main method. Still need to add what happens when you die and the various endings.

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from typing import List
 import colorama as col
 import imageToAscii
 from time import sleep
-from random import randint
+from random import randint, choice
 from os import system
 from time import time
 from itertools import zip_longest
-from playsound import playsound
+from winsound import Beep
 
 col.init(autoreset=True)
 
@@ -16,6 +16,9 @@ class ImageClass:
     dudong = 1
     night = 2
     monster = 3
+
+
+musicNotes = {"C": 262, "D": 294, "E": 330, "F": 349, "G": 392, "A": 440, "B": 494}
 
 
 def pogLog(*aa):
@@ -56,6 +59,7 @@ class HealthPot:
     def __init__(self, healing: float, name: str):
         self.healing = healing
         self.name = name
+
     def __repr__(self):
         return f"Name, {self.name}, Healing {self.healing}"
 
@@ -72,7 +76,7 @@ class Weapon:
 
 class Character:
     def __init__(self, maxPP: int, PP: int, maxHP: int, HP: int, defence: int, inventory: list, friendly: bool,
-                 MC: bool, equippedWeapon: Weapon, alive: bool, name: str):
+                 MC: bool, equippedWeapon: Weapon, alive: bool, name: str, money: int = 0):
         self.maxPP = maxPP
         self.PP = PP
         self.maxHP = maxHP
@@ -84,6 +88,7 @@ class Character:
         self.equippedWeapon = equippedWeapon
         self.alive = alive
         self.name = name
+        self.money = money
 
     def takeDamage(self, damage: float):
         if randint(0, int(self.defence / 10)) == 0:
@@ -122,6 +127,7 @@ class Character:
         for item in self.inventory:
             print(f"{num} :: {item.name}")
             num += 1
+        print(f"Money: {self.money}")
 
     # idea for the inventory system
     # make it a list
@@ -150,7 +156,8 @@ class Character:
         elif isinstance(self.inventory[item], HealthPot):
             self.heal(self.inventory[item].healing)
             del self.inventory[item]
-        elif isinstance(self.inventory[item], HealthPot) and self.inventory[item].name == f"{col.Fore.RED}HEALTH POT OF ORTH":
+        elif isinstance(self.inventory[item], HealthPot) and self.inventory[
+            item].name == f"{col.Fore.RED}HEALTH POT OF ORTH":
             self.heal(self.inventory[item].healing)
 
         # if isinstance(self.inventory[item], Weapon):
@@ -168,13 +175,13 @@ class school():
 
     def __init__(self, char: Character):
         subject = randint(0, 2)
+
         if subject == 0:
             print("You choose to study maths")
             self.maths(char)
-
         elif subject == 1:
             print("You choose to study maths")
-            self.maths(char)
+            self.music(char)
         elif subject == 2:
             print("You choose to study maths")
             self.maths(char)
@@ -216,6 +223,29 @@ class school():
                 print(f"Your answer is correct, but you took too long to answer, you have 5 seconds to answer.")
                 return False
 
+    def music(self, char: Character):
+        print("You choose to study music! ")
+        print("This note is C")
+        Beep(musicNotes["C"], 3000)
+        wait(2)
+        print("What note is this?")
+        noteName, note = choice(list(musicNotes.items()))
+        def listen():
+            Beep(note, 3000)
+            INPUT = input("What note was that? (A, B, C, D, E, F, G), type 'again' to hear again\n> ")
+            if INPUT == noteName:
+                print("Good job, you earned 10 health and 10 PP")
+                char.maxPP += 10
+                char.PP += 10
+
+                char.maxHP += 10
+                char.HP += 10
+            elif INPUT == "again":
+                listen()
+            else:
+                print("Wrong,", end="")
+        listen()
+        print("the note was", noteName, ":", note, "hz")
 
 class day:
     def __init__(self, apparition: bool, eliteApparition: bool = False):
@@ -252,10 +282,11 @@ class battle:
     def __init__(self, *characters: Character) -> None:
         pass
 
-    def start(self, *characters: Character) -> bool:
+    def start(self, mainChar: Character, *characters: Character) -> bool:
 
         self.friendlyCharacters = []
         self.hostileCharacters = []
+        MainCharacter = mainChar
 
         for character in characters:
             if character.friendly:
@@ -333,6 +364,16 @@ class battle:
             result = all(not hC.alive for hC in self.hostileCharacters)
             if result:
                 print("You have defeated all the enemies!")
+                earnedItems = []
+                for hC in self.hostileCharacters:
+                    combinedItems = hC.inventory + [hC.equippedWeapon]
+                    for item in combinedItems:
+                        if randint(0, 1) == 1:
+                            earnedItems.append(item)
+                print(f"You gained new items!")
+                for i in earnedItems:
+                    print(i)
+                MainCharacter.inventory.extend(earnedItems)
                 return True  # if all enemies are dead return True, a victory
             else:
                 pass
@@ -342,12 +383,113 @@ class battle:
             print(f"""{friendlychar.name} : {friendlychar.HP} {' ' * 20} {hostilechar.name} : {hostilechar.HP}""")
 
 
-def printInventory(inventory: dict):
+def showShopInventory(inventory: list):
+    num = 0
     for item in inventory:
-        if item.type == Weapon:
-            print(f"{col.Fore.BLUE}{item[0]} : {item[1]}")
-        else:
-            print(item)
+        print(f"{num} :: {item[0].name}, ${item[1]}")
+        num += 1
+
+
+class weapons:
+    swordOfOrth = Weapon(2000, 1000, f"{col.Fore.RED}SWORD OF ORTH{col.Fore.RESET}")
+    genericSword = Weapon(20, 20, f"Generic Sword")
+    longSword = Weapon(30, 30, "Long Sword")
+    weakSword = Weapon(10, 10, "Weak Sword")
+
+
+class healthPots:
+    healthOfOrth = HealthPot(5000, f"{col.Fore.RED}HEALTH POT OF ORTH{col.Fore.RESET}")
+    largeHealing = HealthPot(100, "Large Healing")
+    mediumHealing = HealthPot(50, "Medium Healing")
+    smallHealing = HealthPot(20, "Small Healing")
+
+
+class town:
+    def __init__(self, MainCharacter: Character):
+        print_lines(
+            "Do you want to go to:",
+            "1: Shop",
+            "2: Blacksmith"
+        )
+
+        self.shopInventory = [(weapons.longSword, 100), (weapons.swordOfOrth, 1000), (healthPots.smallHealing, 10),
+                              (healthPots.mediumHealing, 20), (healthPots.largeHealing, 50)]
+
+        INPUT = input("> ")
+        if INPUT == "1":
+            self.shop(MainCharacter)
+        if INPUT == "2":
+            self.blacksmith(MainCharacter)
+
+    def getPrice(self, itemToSell):
+        if isinstance(itemToSell, Weapon):
+            price = itemToSell.damage + itemToSell.PP
+            return price
+
+        elif isinstance(itemToSell, HealthPot):
+            price = itemToSell.healing / 2
+            return price
+
+    def shop(self, MainCharacter: Character):
+        def startOfShop():
+            print("Welcome to the shop, would you like to buy or sell items? ")
+            INPUT = input("1 : Buy\n2: Sell\n3: exit\n> ")
+            if INPUT == "1":
+
+                print("Here are my wares")
+                showShopInventory(self.shopInventory)
+                print(f"You have ${MainCharacter.money}")
+                selectedItem = input("> ")
+                try:
+                    selectedItem = int(selectedItem)
+                except:
+                    print("use a number number idot")
+                    startOfShop()
+
+                if input(
+                        f"Are you sure you want to buy {self.shopInventory[selectedItem][0]} (y/n) \n> ").lower() == 'y':
+                    if MainCharacter.money >= self.shopInventory[selectedItem][1]:
+                        MainCharacter.inventory.append(self.shopInventory[selectedItem][0])
+                        MainCharacter.money -= self.shopInventory[selectedItem][1]
+                        print(f"You have purchased {self.shopInventory[selectedItem][0]}")
+                        wait(2)
+                        print("Your inventory is now: ")
+                        MainCharacter.showInventory()
+                        startOfShop()
+                    else:
+                        print(
+                            f"Insufficient funds, you have {MainCharacter.money}, you need {self.shopInventory[selectedItem][1]}")
+                        startOfShop()
+
+            if INPUT == '2':
+                print("What do you want to sell?")
+                MainCharacter.showInventory()
+                selectedItem = input("> ")
+                try:
+                    selectedItem = int(selectedItem)
+                except:
+                    print("use a number number idot")
+                    startOfShop()
+
+                if len(MainCharacter.inventory) >= selectedItem and selectedItem >= 0:
+                    if input(
+                            f"Are you sure you want to sell {MainCharacter.inventory[selectedItem]} for {self.getPrice(MainCharacter.inventory[selectedItem])}").lower() == 'y':
+                        MainCharacter.money += self.getPrice(MainCharacter.inventory[selectedItem])
+                        del MainCharacter.inventory[selectedItem]
+                        print("Transaction complete you now have ${}".format(MainCharacter.money))
+                        startOfShop()
+                    else:
+                        startOfShop()
+                else:
+                    print("You don't have that item idot")
+                    startOfShop()
+            if INPUT == '3':
+                return
+
+        startOfShop()
+
+    def blacksmith(self, MainCharacter: Character):
+        pass
 
 
 def main():
@@ -359,13 +501,14 @@ def main():
 
     currentDay = day(True, False)
 
-    MainCharacter = Character(100, 100, 100, 100, 10, [Weapon(20, 20, "Cursed Sword"), HealthPot(20, "Medium Healing")], True, True, Weapon(20, 20),
+    MainCharacter = Character(100, 100, 100, 100, 10, [healthPots.mediumHealing], True, True, weapons.genericSword,
                               True, input("What is your name? "))
 
     if MainCharacter.name.lower() == "orth":
-        MainCharacter.inventory.append(Weapon(69e+42, 1000, f"{col.Fore.RED}SWORD OF ORTH{col.Fore.RESET}"))
-        MainCharacter.inventory.append(HealthPot(5000, f"{col.Fore.RED}HEALTH POT OF ORTH{col.Fore.RESET}"))
+        MainCharacter.inventory.append(healthPots.healthOfOrth)
+        MainCharacter.equippedWeapon = weapons.swordOfOrth
         print(f"{col.Fore.RED}ORTH IS BLESSED BY THE HEAVENS")
+        MainCharacter.inventory.append(weapons.genericSword)
         MainCharacter.showInventory()
         wait(5)
 
@@ -399,14 +542,36 @@ def main():
         sleep(0.4)
         imageToAscii.terminalDefault()
         print("Battle Starting", end="")
-        enemy = Character(100, 100, 100, 100, 10, [], False, False, Weapon(10, 10), True, "enemy dumptruck")
-        enemy2 = Character(100, 100, 100, 100, 10, [], False, False, Weapon(10, 10), True, "the enemy")
+        enemy = Character(100, 100, 100, 100, 10, [healthPots.smallHealing], False, False, weapons.weakSword, True,
+                          "enemy dumptruck")
+        enemy2 = Character(100, 100, 100, 100, 10, [healthPots.smallHealing], False, False, weapons.weakSword, True,
+                           "the enemy")
         friend1 = Character(100, 100, 100, 100, 100, [], True, False, Weapon(20, 10), True, "Xavier")
         wait(2)
-        if not battle().start(MainCharacter, friend1, enemy, enemy2):
-            print("asrtoairesntoarseitnoairsetnoairsentoairsentasorie yyou died lmaiii")
+        battle().start(MainCharacter, MainCharacter, friend1, enemy, enemy2)
 
-    input(vars(MainCharacter))
+        print("Wow that was crazy. I think that was your first time doing that wasn't it?")
+        wait(2)
+        print("You did well, to kill those monsters")
+        wait(2)
+        print("this kind of stuff has been happening for a while now, but only a certain few people")
+        wait(2)
+        print("have been getting transported in the night to some separate world")
+        wait(2)
+        print("The bad thing is that if you die there you die in this world as well")
+        wait(2)
+
+    # if input("Do you want to leave this weirdo school before something like that happens again? (y/n) ")
+
+    while True:
+        currentDay.stepDay()
+        school(MainCharacter)
+        #  a choice menu where you decide where you want to go, e.g. the town, straight home, or "sit and admire the scenery", where they watch a rick astley video for a ton of health
+        town(MainCharacter)  # a town option where you go to buy stuff from a shop etc.
+        # then you go home to sleep and consequently choose yes/no to enter the mirror world
+        # show a bunch of stuff and walk around in the mirror world, if they see a monster they can fight it
+        # if they get 3 of the items that monsters drop, they can use them to challenge the big boi boss monster
+        # if you beat the boss you can finish the game
 
 
 if __name__ == "__main__":
